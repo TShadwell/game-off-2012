a017361e4d874b1181b04727a095a2abb8e7568e
Thomas Shadwell <thomas.shadwell@googlemail.com>
2012-11-08 16:42:08 +0000

    A start, I suppose.

---
+++ script.js
@@ -0,0 +1,21 @@
+(function(){
+	var	canvas = c||document.GetElementById("c");
+		ctx = canvas.getContext("2d"),
+		d=document;
+	
+	//If the screen resizes, the canvas needs to also.
+
+	window.addEventListener(
+		'resize',
+		(function(){
+			ctx.clearRect(
+				0,
+				0,
+				(canvas.width = d.documentElement.clientWidth),
+				(canvas.height = d.documentElement.clientHeight)
+			);
+			return arguments.callee
+		})(),
+		false
+	);
+})();
